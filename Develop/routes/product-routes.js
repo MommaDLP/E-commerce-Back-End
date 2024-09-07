2824ec2a6094d62b8c97e5913542fc5058f2a4fc
MommaDLP <159870485+MommaDLP@users.noreply.github.com>
2024-09-06 21:35:59 -0500

    product-routes

--- Develop/routes/product-routes.js
+++ Develop/routes/product-routes.js
@@ -7,12 +7,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const allProducts = await Product.findAll({ include: [
+      { model: Category },
+      { model: Tag }
+    ]});
+    res.status(200).json(allProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {include: [
+      { model:Category},
+      { model:Tag},
+    ]});
+    res.status(200).json(productData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -48,7 +66,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -92,8 +110,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct= await Product.destory({
+      where: {
+        id:req.params.id
+      }
+    })
+    res.status(200).json(deletedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
